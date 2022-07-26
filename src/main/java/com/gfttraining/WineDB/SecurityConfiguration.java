15135c31227f9a4dc7355c3312eed12ac4c45d76
Mlqo <Mlqo@gft.com>
2022-07-26 10:10:06 +0000

    Resolute-loopLogin

--- src/main/java/com/gfttraining/WineDB/SecurityConfiguration.java
+++ src/main/java/com/gfttraining/WineDB/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.gfttraining.WineDB;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,9 +18,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
        auth.inMemoryAuthentication()
-       	   .withUser("admin").password("123").roles("ADMIN")
+       	   .withUser("admin").password(passwordEncoder().encode("123")).roles("ADMIN")
        	   .and()
-       	   .withUser("user").password("123").roles("USER");
+       	   .withUser("user").password(passwordEncoder().encode("123")).roles("USER");
     }
 	
 	@Override
@@ -28,6 +31,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
        	   .and()
        	   .httpBasic();
     }
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	
 	
