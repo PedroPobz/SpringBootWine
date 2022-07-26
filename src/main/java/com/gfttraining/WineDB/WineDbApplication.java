58c46d30a1010c9f24ce9e3b01e5c19a3d114de4
Pedro Bennàssar Gázquez <pobz@gft.com>
2022-07-26 10:22:31 +0200

    Proyecto inicial

---
+++ src/main/java/com/gfttraining/WineDB/WineDbApplication.java
@@ -0,0 +1,13 @@
+package com.gfttraining.WineDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfttraining/WineDB/WineDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfttraining.WineDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
