af25e66fa373dcd0930ccffac7a6985a580385fb
Miguel <mlqo@gft.com>
2022-07-28 10:04:32 +0200

    First commit

---
+++ src/main/java/com/gfttraining/WineDB/Service/RegionService.java
@@ -0,0 +1,29 @@
+package com.gfttraining.WineDB.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.gfttraining.WineDB.Model.Region;
+import com.gfttraining.WineDB.Repository.RegionRepository;
+
+@Service
+public class RegionService {
+    @Autowired
+    RegionRepository regionRepository;
+
+    public Region findById(int id){
+        return regionRepository.findById(id).get();
+    }
+
+    public Region createRegion(Region region){
+       return regionRepository.save(region);
+    }
+
+    public void deleteRegion(int id){
+        regionRepository.deleteById(id);
+    }
+}
